67061b08eae006249db36de7a280996c23e788ad
Andor L <andorlavin@hotmail.com>
2023-01-19 19:10:38 +0100

    Revert ":rocket: Release: 1.44.4"
    This reverts commit db18347952c61c07497bbd33340ae7c5a710231b.

--- apps/client/src/components/nav/dropdowns/OfficerDropdown.tsx
+++ apps/client/src/components/nav/dropdowns/OfficerDropdown.tsx
@@ -96,11 +96,11 @@ export function OfficerDropdown() {
         <Button
           role="listitem"
           className={classNames(isActive("/officer") && "font-semibold")}
-          variant="transparent"
+          variant="bar"
         >
           {t("officer")}
           <span className="mt-1 ml-1">
-            <ChevronDown width={15} height={15} className="text-gray-700 dark:text-gray-300" />
+            <ChevronDown width={15} height={15} className="text-white dark:text-white" />
           </span>
         </Button>
       }
